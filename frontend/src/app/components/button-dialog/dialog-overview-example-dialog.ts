09ceb2ed29611115e06f10edd9c9cd28eb25a46d
mikifriki <mihkel.liblikman@gmail.com>
2020-11-20 04:04:10 +0200

    Created modal component for use with delete and checkout.

---
+++ frontend/src/app/components/button-dialog/dialog-overview-example-dialog.ts
@@ -0,0 +1,31 @@
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {FavoritesService} from "../../services/favorites.service";
+
+@Component({
+	selector: 'dialog-overview-example-dialog',
+	templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+	constructor(
+		public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+		@Inject(MAT_DIALOG_DATA) public data: DialogData,
+		private favoritesService: FavoritesService
+	) {
+	}
+
+	onNoClick(): void {
+
+		this.dialogRef.close();
+	}
+
+	confimClick() {
+		this.favoritesService.sendUpdate(`Confirmed`);
+	}
+
+}
+
+export interface DialogData {
+	calledEvent: string;
+}
